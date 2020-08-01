98a0079b5ddc0998b4dbd8982a3ccfa40744f106
Connor Waldoch <cwal37@gmail.com>
2020-08-01 12:35:22 -0400

    An actual chart
    also discovered missing years, need to go back to the data roll script to figure that one out

--- chart_data.py
+++ chart_data.py
@@ -12,34 +12,51 @@ import pdb
 
 df = pd.read_csv('all_degrees_data.csv')
 #df = df.astype(str)
+# removing the "all" categories still in the data
+df = df[df['cipcode'] != 99000]
 
-dfMap = pd.read_csv('collated_data_dict.csv')
+dfMap = pd.read_csv('degree_family_mapping_complete.csv')
 #dfMap = dfMap.astype(str)
 
-"""
-oh god, the underlying data has a bunch of nines, it's unrounded, need to think
-about a solution to this, not sure how easy it will be or not, don't
-have a great solution right now. . . .
-
-eh, manual fixes and additions also not working great, something is up with the
-way this dat ahas been stored, what a hassle.
-"""
-
-
-degreeDict = dict(zip(dfMap['codevalue'], dfMap['valuelabel']))
-
-degreeNames = []
-uCodes = list(set(df['cipcode']))
-
-for uCode in uCodes:
-    if uCode < 100:
-        try:
-            n = degreeDict[uCode]
-        except KeyError:
-            if len(str(uCode)) >7:
-                uCode = np.round(uCode,3)
-                n = degreeDict[uCode]
-            else:
-                pdb.set_trace()
+def year_fix(year):
+    #pdb.set_trace()ye
+    if year <10:
+        year = int('200'+str(year))
+    elif year <80:
+        year = int('20'+str(year))
+    else:
+        year = int('19'+str(year))
         
-#df['code_name'] = [degreeDict[x] for x in df['cipcode'].values]
+    return(year)
+
+df['year2'] = [year_fix(x) for x in df['year'].values]
+
+
+dfMap = dfMap[dfMap['cipName'].isna() == False]
+
+dfMap['upName'] = [x.upper() for x in dfMap['cipName'].values]
+
+famDict1 = dict(zip(dfMap['upName'].values, dfMap['my_cat1'].values))
+famDict2 = dict(zip(dfMap['upName'].values, dfMap['my_cat2'].values))
+
+df['fam1'] = [famDict1[x.upper()] for x in df['name'].values]
+df['fam2'] = [famDict2[x.upper()] for x in df['name'].values]
+
+dfBach = df[df['awlevel'] == 5]
+
+dfPivotFam1 = pd.pivot_table(dfBach, values = 'CTOTALT', index=['year2'],
+                             columns=['fam1'], aggfunc = np.sum)
+dfPivotFam2 = pd.pivot_table(dfBach, values = 'CTOTALT', index=['year2'],
+                             columns=['fam2'], aggfunc = np.sum)
+
+
+fig = plt.figure(figsize=(14,8))
+
+for col in dfPivotFam2:
+    
+    plt.scatter(dfPivotFam2.index, dfPivotFam2[col], label = col)
+    
+#plt.legend(ncol=3)
+plt.legend(ncol=3,loc='lower center', bbox_to_anchor=(0.5, -0.4))
+plt.savefig('first_scatter_out.png', dpi=300)
+#plt.show()
